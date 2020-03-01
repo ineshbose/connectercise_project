a7afd7a511dd8ef5d2c58144f38a1ad9e8a5892e
Marc Auf der Heyde <2326744a@student.gla.ac.uk>
2020-03-01 11:22:08 +0000

    Changed views to return correct templates. Added static/css/main.css to hold CSS for main application. Made background colour dark grey for all templates using master.html

--- main/views.py
+++ main/views.py
@@ -7,15 +7,15 @@ def index(request):
 
 def about(request):
     context_dict = {'message': 'Hello Marc.'}
-    return render(request, 'main/index.html', context=context_dict)
+    return render(request, 'main/about.html', context=context_dict)
 
 def userpage(request):
     context_dict = {'message': 'Hello Inesh.'}
-    return render(request, 'main/index.html', context=context_dict)
+    return render(request, 'main/user.html', context=context_dict)
 
 def userBookmarks(request):
     context_dict = {'message': 'Hello (again) Inesh.'}
-    return render(request, 'main/index.html', context=context_dict)
+    return render(request, 'main/bookmarks.html', context=context_dict)
 
 def explore(request):
     context_dict = {'message': 'Hello Toby.'}
