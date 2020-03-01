8be587a846dc0fe769321a70bd825d65f2babe96
Marc Auf der Heyde <2326744a@student.gla.ac.uk>
2020-03-01 14:51:14 +0000

    Changed title pages passed in context dictionary. Added more CSS for about page. Last commit to master

--- main/views.py
+++ main/views.py
@@ -2,21 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'message': 'Hello Andrea.'}
+    context_dict = {'message': 'Index'}
     return render(request, 'main/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {'message': 'Hello Marc.'}
+    context_dict = {'message': 'About'}
     return render(request, 'main/about.html', context=context_dict)
 
 def userpage(request):
-    context_dict = {'message': 'Hello Inesh.'}
+    context_dict = {'message': 'User Page'}
     return render(request, 'main/user.html', context=context_dict)
 
 def userBookmarks(request):
-    context_dict = {'message': 'Hello (again) Inesh.'}
+    context_dict = {'message': 'Bookmarks'}
     return render(request, 'main/bookmarks.html', context=context_dict)
 
 def explore(request):
-    context_dict = {'message': 'Hello Toby.'}
-    return render(request, 'main/index.html', context=context_dict)
+    context_dict = {'message': 'Explore'}
+    return render(request, 'main/explore.html', context=context_dict)
