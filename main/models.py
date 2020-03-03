db58a623472ab0f33168107c4b02ce77713e8e9e
Marc Auf der Heyde <2326744a@student.gla.ac.uk>
2020-03-03 12:37:35 +0000

    Made username a primary key in UserProfile, and sessionID a primary key in SportingRequest

--- main/models.py
+++ main/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class UserProfile(models.Model):
-    username = models.CharField(max_length=128, unique=True)
+    username = models.CharField(max_length=128, primary_key=True)
     password = models.CharField(max_length=64)
-    email = models.CharField(max_length=64)
+    email = models.CharField(max_length=64,unique=True)
     pageURL = models.CharField(max_length=128)
     sport = models.CharField(max_length=128)
     location = models.CharField(max_length=128)
-    socialMedia = models.CharField(max_length=128)
+    socialMedia = models.CharField(max_length=128, unique=True)
 
     def __str__(self):
         return self.username
@@ -16,7 +17,7 @@ class UserProfile(models.Model):
 class SportingRequest(models.Model):
     time = models.DateTimeField(auto_now=True)
     location = models.CharField(max_length=128)
-    sessionID = models.CharField(max_length=16,unique=True)
+    sessionID = models.CharField(max_length=16,primary_key=True)
     sport = models.CharField(max_length=128)
 
     def __str__(self):
