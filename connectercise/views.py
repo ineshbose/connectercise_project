045c9523aaf4a491981fb316dcaacaca65b2542e
Marc Auf der Heyde <2326744a@student.gla.ac.uk>
2020-03-09 16:56:55 +0000

    Fixed registration setup. Old views removed and unused templates

--- connectercise/views.py
+++ connectercise/views.py
@@ -74,55 +74,9 @@ def add_request(request, sport_name_slug):
     context_dict = {'form': form, 'sport': sport}
     return render(request, 'connectercise/add_request.html', context=context_dict)
 
-
-def register(request):
-    registered = False
-    if request.method == 'POST':
-        user_form = UserForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
-
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-            profile.save()
-            registered = True
-        else:
-            print(user_form.errors, profile_form.errors)
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileForm()
-    return render(request, 'connectercise/register.html', context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
-
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-        if user:
-            if user.is_active:
-                login(request, user)
-                return redirect(reverse('connectercise:index'))
-            else:
-                return HttpResponse("Your account is disabled.")
-        else:
-            print(f"Invalid login details: {username}, {password}")
-            return HttpResponse("Invalid login details supplied.")
-    else:
-        return render(request, 'connectercise/login.html')
-
 @login_required
 def restricted(request):
     return render(request, 'connectercise/restricted.html')
-
-@login_required
-def user_logout(request):
-    logout(request)
-    return redirect(reverse('connectercise:index'))
 
 def get_server_side_cookie(request, cookie, default_val=None):
     val = request.session.get(cookie)
