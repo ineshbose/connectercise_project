d38eed51eab9a1b32cc9e25830a3fd7d1c94dab3
Andrea Diaz Aguiar <2396765d@student.gla.ac.uk>
2020-03-13 15:00:04 +0000

    started add request form

--- connectercise_project/settings.py
+++ connectercise_project/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'connectercise',
     'registration',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 RGISTRATION_OPEN = True
 
