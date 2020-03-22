42a5ad7d225ae9db962331deaf4b86dbccb81bd0
ineshbose <ineshbose@yahoo.com>
2020-03-22 15:46:22 +0000

    Revert "Trying chat"
    This reverts commit 58c5c347011b0aaffa7050dfd65851693132c8ab.

--- chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from chat import views
-
-app_name = 'chat'
-
-urlpatterns = [
-    path('', views.all_chats, name='all_chats'),
-]
--- connectercise_project/settings.py
+++ connectercise_project/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'connectercise',
     'registration',
     'crispy_forms',
-    'chat',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
