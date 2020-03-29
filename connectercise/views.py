0651549e6f2553b8658419f89cd2c563514699e0
ineshbose <ineshbose@yahoo.com>
2020-03-29 15:47:13 +0100

    Views cleanup

--- connectercise/views.py
+++ connectercise/views.py
@@ -16,14 +16,12 @@ def index(request):
     context_dict['boldmessage'] = 'lorem ipsum?'
     context_dict['sports'] = sport_list
     context_dict['requests'] = request_list
-    visitor_cookie_handler(request)
     return render(request, 'connectercise/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Team Connectercise!'}
     sport_list = Sport.objects.order_by('-likes')[:5]
+    context_dict = {}
     context_dict['sports'] = sport_list
-    visitor_cookie_handler(request)
     context_dict['visits'] = request.session['visits']
     return render(request, 'connectercise/about.html', context=context_dict)
 
@@ -31,15 +29,13 @@ def activity(request):
     sport_list = Sport.objects.order_by('-likes')[:5]
     request_list = SportRequest.objects.order_by('-views')[:5]
     context_dict = {}
-    context_dict['boldmessage'] = 'lorem ipsum?'
     context_dict['sports'] = sport_list
     context_dict['requests'] = request_list
-    visitor_cookie_handler(request)
     return render(request, 'connectercise/activity.html', context=context_dict)
 
 def sports(request):
     sport_list = Sport.objects.order_by('-likes')[:5]
-    context_dict = {'boldmessage': 'This tutorial has been put together by Team Connectercise!'}
+    context_dict = {}
     context_dict['sports'] = sport_list
     return render(request, 'connectercise/sports.html', context=context_dict)
 
@@ -149,24 +145,6 @@ def restricted(request):
     sport_list = Sport.objects.order_by('-likes')[:5]
     context_dict['sports'] = sport_list
     return render(request, 'connectercise/restricted.html', context=context_dict)
-
-def get_server_side_cookie(request, cookie, default_val=None):
-    val = request.session.get(cookie)
-    if not val:
-        val = default_val
-    return val
-
-def visitor_cookie_handler(request):
-    visits = int(get_server_side_cookie(request, 'visits', '1'))
-    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
-
-    if (datetime.now() - last_visit_time).days > 0:
-        visits = visits+1
-        request.session['last_visit'] = str(datetime.now())
-    else:
-        request.session['last_visit'] = last_visit_cookie
-    request.session['visits'] = visits
 
 def show_user(request, user_profile_slug):
     context_dict = {}
