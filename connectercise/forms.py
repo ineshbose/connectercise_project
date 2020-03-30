0e3a71dac415c329700110eb02632c280ea85998
ineshbose <ineshbose@yahoo.com>
2020-03-30 18:21:27 +0100

    Datetime form fixed

--- connectercise/forms.py
+++ connectercise/forms.py
@@ -17,6 +17,7 @@ class RequestForm(forms.ModelForm):
     desc = forms.CharField(help_text="Please enter a description.", widget=forms.Textarea)
     #suggested_time = forms.DateTimeField(help_text="Enter a suggested time (optional).", required=False)
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    suggested_date = forms.DateTimeField(widget=forms.SelectDateWidget(), required=False)
 
     class Meta:
         model = SportRequest
@@ -31,6 +32,8 @@ class SportRequestForm(forms.ModelForm):
     desc = forms.CharField(help_text="Please enter a description.", widget=forms.Textarea)
     #suggested_time = forms.DateTimeField(help_text="Enter a suggested time (optional).", required=False)
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    suggested_date = forms.DateTimeField(widget=forms.SelectDateWidget(), required=False)
+
 
     class Meta:
         model = SportRequest
